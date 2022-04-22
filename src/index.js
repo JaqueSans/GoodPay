b257f44ed87910bc953917802b304ec6fa9d29e6
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-04-22 12:03:13 -0300

    Atualizacao adição de pagina de promoções

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function rotaCadastro(){
+var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
 
+function rotaCadastro() {
+  window.location.href = "../registro.html";
+}
+function rotaRecuperar() {
+  window.location.href = "../recuperar.html";
+}
+function rotaLogin() {
+  let email = document.getElementById("email").value;
+  let senha = document.getElementById("senha").value;
+  if (email === "victoricoma@gmail.com" && senha === "gordinho123") {
+    window.location.href = "../index.html";
+  } else {
+    janelaLogin.show();
+  }
 }
 
-function rotaRecuperar(){
+function alertaPromo(){
+  let titulo ="Promomção 1 [Quebra da Banca]";
+  let corpo = "<h6>Super legal</h6> <p><img src='./img/4.png' width='160' height='160'></p> ";
 
-}
-function rotaLogin(){
-    
+  janelaPromocao.show();
+
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("corpo").innerHTML = corpo; 
 }
